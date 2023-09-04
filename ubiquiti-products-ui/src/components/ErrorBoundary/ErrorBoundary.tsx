c6cf7fac4116a1006f83619b9c2b8bd2171a8b83
Dom Villegas <domvillgithub@gmail.com>
2023-09-03 20:09:59 -0700

    Fix build

--- ubiquiti-products-ui/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ ubiquiti-products-ui/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
+  // IMPROVE THIS TYPE
+  // IMPROVE THIS TYPE
+  // IMPROVE THIS TYPE
+  // IMPROVE THIS TYPE
+  constructor(props: any) {
     super(props);
 
-    // Define a state variable to track whether is an error or not
     this.state = { hasError: false };
   }
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI
-
     return { hasError: true };
   }
   componentDidCatch(error, errorInfo) {
-    // You can use your own error logging service here
     console.log({ error, errorInfo });
   }
   render() {
-    // Check if the error is thrown
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return (
         <div>
           <h1>An Error Occured</h1>
@@ -36,8 +34,6 @@ class ErrorBoundary extends React.Component {
         </div>
       );
     }
-
-    // Return children components in case of no error
 
     return this.props.children;
   }
